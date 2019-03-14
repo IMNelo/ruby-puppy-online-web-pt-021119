feba2656e6444baec5884141d5a4b67463adf12b
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-03-14 04:17:56 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+  
+  @@all = [] #<-Class Variable, stores new instance of a Dog
+  
+  attr_accessor :name
+
+  
+  def initialize(name)  #<-Instantiates new instances
+    @name = name
+    @@all << self #<-Shovels new instance of Dog to @@all=[] empty array
+
+  end
+  
+  def self.all  #<-Class Method, exposes new instance of Dog
+    @@all.each do |dog_name|
+      puts dog_name.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+  
+end
